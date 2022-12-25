14755d4199b5e5a49bbb205f3826d3f1bf23e470
walter-dev <Abdullabek710@gmail.com>
2022-12-25 14:16:45 +0500

    Quiz App

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+        prompt:"What is 1+3?",
+        optionA:"24",
+        optionB:"12",
+        optionC:"32",
+        optionD:"4",
+        answer:"D"
+    },
+    {
+        prompt:"Which is a Streaming Platform?",
+        optionA:"Twitter",
+        optionB:"Youtube",
+        optionC:"Instagram",
+        optionD:"Telegram",
+        answer:"B"
+    },
+    {
+        prompt:"Who is Shavkat Mirziyoyev",
+        optionA:"President of Uzbekistan",
+        optionB:"President of USA",
+        optionC:"President of China",
+        optionD:"President of Russian",
+        answer:"A"
+    },
+    {
+        prompt:"Best tech Youtube Channel?",
+        optionA:"PedroTech",
+        optionB:"LamaDev",
+        optionC:"Hallden",
+        optionD:"Saud Abdulwahed",
+        answer:"B"
+    }
+];
